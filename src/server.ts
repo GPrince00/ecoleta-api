d99ac78da5d3efed494cfa4c396b894cea0d6922
GPrince00 <gustavoprince0@gmail.com>
2020-06-09 22:36:06 -0300

    feat(routes): creates routes file

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,10 @@
 import express, { request, response } from 'express';
+import routes from './routes';
 
 const  app = express();
 
 app.use(express.json());
 
-const users = [
-    'Prince',
-    'Franscisca',
-    'Marta',
-    'Josefa'
-];
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filteredUsers);
-});
-
-app.get('/users:id', (request, response) => {
-    const id = Number(request.params.id);
-    
-    const user = users[id];
-
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    const user = {
-        name: data.name,
-        email: data.email,
-    };
-
-    return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
